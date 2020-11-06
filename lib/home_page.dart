0af9d35893e5103dadc5e9f5b717371857f2868f
EnesCeltikk <enes.celtik@hotmail.com>
2020-11-07 00:23:36 +0300

    BottomNavigatorBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -24,6 +24,8 @@ class _HomePageState extends State<HomePage> {
     _events = {};
     _eventController = TextEditingController();
     _selectedEvents = [];
+   
+
     initPrefs();
   }
 
@@ -64,42 +66,7 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontSize: 20, color: Colors.black),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
-        selectedFontSize: 20,
-        unselectedFontSize: 14,
-        // backgroundColor: Colors.orange[300],
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.calendar_today,
-              size: 25,
-            ),
-            backgroundColor: Colors.pink,
-            label: "Günlük",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.calendar_today,
-              size: 25,
-            ),
-            label: "Haftalık",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.calendar_today,
-              size: 25,
-            ),
-            label: "Aylık",
-          )
-        ],
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
+      
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
